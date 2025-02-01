3f204ec45ee3ca4c561f071a9a984f38fbe20c8d
Temur Turan <mustafa.temur.turan@gmail.com>
2025-02-01 19:50:19 +0300

    Kamera denetimi yapıldı, karakter eklendi, oyun parkuru uzatıldı

---
+++ Oyun Dosyaları/Assets/Kodlar/Yanma.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Yanma : MonoBehaviour
+{
+  private void OnTriggerEnter (Collider other)
+    {
+        //ölme durumu
+    }
+
+}
--- Oyun Dosyaları/Assets/Kodlar/karakterkontrol.cs
+++ Oyun Dosyaları/Assets/Kodlar/karakterkontrol.cs
@@ -6,20 +6,36 @@ public class karakterkontrol : MonoBehaviour
 {
     public float hiz = 10f;
     public float ziplama = 3f;
-    private void Start()
+    private Animator animator;
+
+    void Start()
     {
+        animator = GetComponent<Animator>();
     }
 
-      void Update()
+    void Update()
+    {
+        float zYonu = Input.GetAxis("Vertical") * hiz;
+        float xYonu = Input.GetAxis("Horizontal") * hiz;
+        float yYonu = 0f;
+
+        if (Input.GetButtonDown("Jump"))
         {
-            float zYonu = Input.GetAxis("Vertical") * hiz;
-            float xYonu = Input.GetAxis("Horizontal") * hiz;
-            float yYonu = Input.GetAxis("Jump") * ziplama;
-            zYonu *= Time.deltaTime;
-            xYonu *= Time.deltaTime;
-            yYonu *= Time.deltaTime;
+            yYonu = ziplama;
+        }
+
+        zYonu *= Time.deltaTime;
+        xYonu *= Time.deltaTime;
+        yYonu *= Time.deltaTime;
+
+        transform.Translate(xYonu, yYonu, zYonu);
+
+        if (animator != null)
+        {
+            animator.SetFloat("OyuncuV", zYonu);
+            animator.SetFloat("OyuncuY", yYonu);
 
 
-        transform.Translate(xYonu,yYonu, zYonu);
         }
     }
+}
